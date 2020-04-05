2d66c9deb00680fd72c6e058122df7b7f786708e
roshnirajwani <dollrosh320@gmail.com>
2020-04-05 15:39:51 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -233,13 +233,13 @@ $('document').ready(function(){
 		$('.bannar').removeClass('bannar-come');
 		 $('.collage').fadeIn(2000).promise().done(function(){
 			 $('.picmsg').fadeIn(3000).promise().done(function(){
-				 $("#pic_msg").fadeIn('fast').delay(4000).promise().done(function() { 
+				 $("#pic_msg").fadeIn('fast').delay(5000).promise().done(function() { 
 					$("#pic_msg").fadeOut(4000).delay(3000).promise().done(function(){ 
-					 $('.collage').fadeOut(6000).promise().done(function () { 
+					 $('.collage').fadeOut(5000).delay(1000).promise().done(function () { 
 
 					  $('.lastpic').fadeIn(2000).promise().done(function() { 
 						  $('.lastgreet').fadeIn(3000).promise().done(function(){
-							 $("#firstmsg").fadeIn('fast').promise().done(function(){
+							 $("#firstmsg").css({top: 200px}).fadeIn('fast').promise().done(function(){
 								$("#firstmsg").fadeOut('slow').delay(4000).promise().done(function(){ 
 								$("#secondmsg").fadeIn('fast').promise().done(function(){
 								$("#secondmsg").fadeOut('slow').delay(4000).promise().done(function(){
